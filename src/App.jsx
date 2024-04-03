8751793a160d7f59dccd6c0f4db3c62851c6ed92
BOrekhova <barbaraorekhova@gmail.com>
2024-04-03 22:30:25 +0300

    start components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -6,17 +7,15 @@ import './App.css'
 import React, { Component } from 'react';
 import { Editor } from "react-draft-wysiwyg";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import Algebra from './Algebra'
 
 function App() {
   return (
-      <Editor
-        editorState={editorState}
-        toolbarClassName="toolbarClassName"
-        wrapperClassName="wrapperClassName"
-        editorClassName="editorClassName"
-        onEditorStateChange={this.onEditorStateChange}
-      />
+    <div>
+      <h1>Welcome to My App</h1>
+      <Algebra color="red" />
+    </div>
   )
 }
 
-export default App
+export default App;
