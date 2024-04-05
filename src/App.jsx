9ed4be740e4aa8a32b37fb9cffdb12056eba1bbb
BOrekhova <barbaraorekhova@gmail.com>
2024-04-05 21:36:46 +0300

    try to make components button

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,39 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-// import Facts from './Facts.jsx'
+import Button from './components/Button.jsx'
 
 import React, { Component } from 'react';
-// import { Editor } from "react-draft-wysiwyg";
+import { Editor } from "react-draft-wysiwyg";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
-function Facts(props) {
-  return (
-    <>
-      <h2>These are {props.theme} facts!</h2>
-      <p>{props.fact}</p>
-    </>
-  );
-};
+// function Facts(props) {
+//   return (
+//     <>
+//       <h2>These are {props.theme} facts!</h2>
+//       <p>{props.fact}</p>
+//     </>
+//   );
+// };
+
+// function Showthemes(props) {
+//   return (
+//     <>
+//       <h2>These are {props.theme} facts!</h2>
+//       <p>{props.fact}</p>
+//     </>
+//   );
+// }
 
 function App() {
   return (
     <>
     <h1>hi! this is math facts!</h1>
-    <Facts theme="Algebra" fact="I am an Algebra"/>
-    <Facts theme="Geometry" fact="I am an Algebra"/>
-    <Facts theme="Combinatorics" fact="I am an Algebra"/>
-    <Facts theme="Trigonometry" fact="I am an Algebra"/>
+    <Button theme="Algebra"/>
+    <Button theme="Geometry"/>
+    <Button theme="Combinatorics"/>
+    <Button theme="Trigonometry"/>
     </>
   )
 }
