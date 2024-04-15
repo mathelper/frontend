80f1300d1fda8101b510c87b05dcebe8311e1ead
elintendo <elintendo@yandex.ru>
2024-04-15 09:41:16 +0300

    add .env file

--- frontend/src/components/FactDetails.jsx
+++ frontend/src/components/FactDetails.jsx
@@ -5,11 +5,11 @@ import { useHistory, useParams } from 'react-router-dom';
 const FactDetails = () => {
 
     const { id } = useParams();
-    const { data: fact, isPending, error } = useFetch('http://localhost:8000/facts/' + id);
+    const { data: fact, isPending, error } = useFetch(process.env.VITE_BASE_URL + id);
     const history = useHistory();
 
     const handleClick = () => {
-        fetch('http://localhost:8000/facts/' + fact.id, {
+        fetch(process.env.VITE_BASE_URL+ fact.id, {
             method: 'DELETE'
         }).then(() => {
             history.push('/');
