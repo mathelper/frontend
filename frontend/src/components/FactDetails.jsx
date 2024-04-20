b277a20e339a27dc87d83da86e2da222220c478e
barbara <56512329+BOrekhova@users.noreply.github.com>
2024-04-21 00:39:49 +0300

    update factdetails

--- frontend/src/components/FactDetails.jsx
+++ frontend/src/components/FactDetails.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import useFetch from './useFetch';
 import { useHistory, useParams } from 'react-router-dom';
+import Latex from 'react-latex';
 
 const FactDetails = () => {
 
     const { id } = useParams();
-    const { data: fact, isPending, error } = useFetch(import.meta.env.VITE_BASE_URL + id);
-    const history = useHistory();
+    const { data: fact, isPending, error } = useFetch('http://localhost:8000/facts/' + id);
+    // const history = useHistory();
 
-    const handleClick = () => {
-        fetch(import.meta.env.VITE_BASE_URL+ fact.id, {
-            method: 'DELETE'
-        }).then(() => {
-            history.push('/');
-        })
-    }
+    // const handleClick = () => {
+    //     fetch('http://localhost:8000/facts/' + fact.id, {
+    //         method: 'DELETE'
+    //     }).then(() => {
+    //         history.push('/');
+    //     })
+    // }
 
     return (
         <div className="fact-details">
@@ -23,8 +24,8 @@ const FactDetails = () => {
             { fact && (
                 <article>
                     <h2>{ fact.title }</h2>
-                    <div>{ fact.body }</div>
-                    <button onClick={handleClick}>delete</button>
+                    <div><Latex>{fact.body}</Latex></div>
+                    {/* <button onClick={handleClick}>delete</button> */}
                 </article>
             )}
         </div>
